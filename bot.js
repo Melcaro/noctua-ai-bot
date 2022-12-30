8c203d9e1979322f4f6c94e15cbbce42af70a55f
Melcaro <melanie.caro78@gmail.com>
2022-12-30 20:06:12 +0100

    update config for server

--- bot.js
+++ bot.js
@@ -1,8 +1,14 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
 import { ask } from './ai.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const client = new Client({
   intents: [
